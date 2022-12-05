b9ec4d03359ff03cdce034c13934f641776a1154
EvaXausa <xausa.eva@gmail.com>
2022-12-05 18:57:59 +0100

    questions

--- js/script.js
+++ js/script.js
@@ -1,15 +1,23 @@
 console.log("JS OK");
 
 /*
-chiedere all'utente il numero di chilometri che vuole percorrere
-chiedere l'eta 
-calcolare il prezzo del viaggio
+- 1 chiedere all'utente il numero di chilometri che vuole percorrere
+- 2 chiedere l'eta 
+- 3 calcolare il prezzo del viaggio
+- 4 applica il 20% di sconto se minorenne 1l 40% sugli over 65
 */
 
 // Recupero l'elemento in pagina
 const targetElement = document.getElementById("target");
 
-//Domando 1-2
-const userKilometers = prompt("Quanti kilometri vuoi percorrere?");
+//Domanda 1-2
+const userKilometers = prompt("Quanti kilometri vuoi percorrere?", 50);
 const userAge = prompt("Quanti anni hai?");
 console.log(userChoise, userNumber);
+
+//applica il 20% di sconto se minorenne 1l 40% sugli over 65
+if (userAge < 18) {
+  //
+} else {
+  //
+}
