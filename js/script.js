b5027d393162d7bf5078aab25340fce95e54d956
EvaXausa <xausa.eva@gmail.com>
2022-12-05 16:33:39 +0100

    files

---
+++ js/script.js
@@ -0,0 +1,15 @@
+console.log("JS OK");
+
+/*
+chiedere all'utente il numero di chilometri che vuole percorrere
+chiedere l'eta 
+calcolare il prezzo del viaggio
+*/
+
+// Recupero l'elemento in pagina
+const targetElement = document.getElementById("target");
+
+//Domando 1-2
+const userKilometers = prompt("Quanti kilometri vuoi percorrere?");
+const userAge = prompt("Quanti anni hai?");
+console.log(userChoise, userNumber);
